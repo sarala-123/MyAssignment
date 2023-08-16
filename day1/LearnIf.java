d53617c44c3a286cb4030052a06f408d32d005b8
sarala-123 <142376169+sarala-123@users.noreply.github.com>
2023-08-16 10:40:35 +0530

    Add files via upload

---
+++ day1/LearnIf.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class LearnIf {
+
+	public static void main(String[] args) {
+        int mark =70;
+        
+        if(mark<=70) {
+            System.out.println("fail");
+        }
+        else if(mark==80) {
+            System.out.println("Grade E");
+        
+        }
+        else {
+            System.out.println("Enter the mark");
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
+
---
+++ day1/Mobile.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO -generated method stub
+String name="Vivo y21";
+int rate=800000;
+char lego='S';
+float weight=33.56f;
+System.out.println("The company name is"+name);
+System.out.println("Phone rate"+ rate);
+System.out.println("Company lego" +lego);
+System.out.println("Phone weight" +weight);
+
+
+	
+}
+	}
+
+
